08bef0a60aa6aba3e3af835298ac4f91742d66e5
OF@TEIN <ahmadrathore@gmail.com>
2019-09-27 19:36:24 +0900

    Update ping_latency.sh

--- basic_functionality/Measurement/docker/ping_latency.sh
+++ basic_functionality/Measurement/docker/ping_latency.sh
@@ -35,7 +35,7 @@
 
 
 #VCenter
-	my_list=`cat  /opt/measurement/micro-box-config.yaml`
+	my_list=`cat  micro-box-config.yaml`
 for item in $my_list;
 do
   if [[ $item == *"VCENTER"* ]]; then
@@ -59,7 +59,7 @@ BOX_HOSTNAME=${BOX_HOSTNAME:7}
 
 ping_grid=""
 
-	my_list=`cat  /opt/measurement/micro-box-config.yaml`
+	my_list=`cat  micro-box-config.yaml`
 
 echo "**********************************************************************"
 echo "**********************************************************************"
@@ -92,7 +92,7 @@ function micro_box_ping_latency {
 
 
 
-#	cat  /opt/measurement/micro-box-config.yaml | while read line
+#	cat  micro-box-config.yaml | while read line
 	while read line
         do
 #         TO_BOX="cut -d ":" -f 2 <<< $line1
@@ -167,7 +167,7 @@ function micro_box_ping_latency {
 	  echo -e $ping
           echo -e $latency
           echo -e "\n\n--------------------------------------------------------------------------------------------------------------\n\n"
-        done < <(cat  /opt/measurement/micro-box-config.yaml )
+        done < <(cat  micro-box-config.yaml )
 
         echo -e $ping
         echo -e $latency
